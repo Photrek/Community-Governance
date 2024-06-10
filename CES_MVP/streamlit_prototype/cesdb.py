2e113233890f9a48bc0cb1f7c96f9fe395083f1a
Roman Preuß <romanpre@gmail.com>
2024-06-10 21:07:01 +0200

    Add levenstein string similarity function
    Used to match proposals from voting portal and proposal portal
    based on proposal title

--- CES_MVP/streamlit_prototype/cesdb.py
+++ CES_MVP/streamlit_prototype/cesdb.py
@@ -11,6 +11,48 @@ def get_db_connection() -> duckdb.DuckDBPyConnection:
     con.sql("CREATE SCHEMA IF NOT EXISTS db;")
     con.sql("USE db;")
 
+    create_function(con, 'levenshtein', levenshtein)
+
     # if 'db_connection' not in st.session_state:
     #     st.session_state['db_connection'] = con
     return con
+
+def levenshtein(a, b) -> int:
+    """
+    Calculate the Levenshtein distance between two strings.
+    """
+    
+    if len(a) < len(b):
+        return levenshtein(b, a)
+    if len(b) == 0:
+        return len(a)
+    prev_row = range(len(b) + 1)
+    for i, c1 in enumerate(a):
+        current_row = [i + 1]
+        for j, c2 in enumerate(b):
+            insertions = prev_row[j + 1] + 1
+            deletions = current_row[j] + 1
+            substitutions = prev_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        prev_row = current_row
+    return prev_row[-1]
+
+def create_function(con, function_name, function):
+    """
+    Create a function in DuckDB.
+    """
+    function_check = f"""SELECT DISTINCT  function_name
+                        FROM duckdb_functions()
+                        WHERE lower(function_type) = 'scalar'
+                        AND lower(function_name) in ('{function_name}')
+                        ORDER BY function_name;"""
+
+    function_check_output = con.query(function_check)
+    try:
+        if not function_check_output:
+            con.create_function(function_name, function)
+            print(f"Function '{function_name}' created successfully.")
+    except (duckdb.Error, ValueError) as error:
+        raise ValueError(
+            f"Failed to create function '{function_name}': {str(error)}"
+        ) from error
--- CES_MVP/streamlit_prototype/pages/1_🗂️_Data_Explorer.py
+++ CES_MVP/streamlit_prototype/pages/1_🗂️_Data_Explorer.py
@@ -9,6 +9,24 @@ con = cesdb.get_db_connection()
 Explore all tables in the local database.
 """
 
+proposal_similarity_query = """
+SELECT
+    t1.question_id AS id1,
+    t1.question AS title1,
+    t2.id AS id2,
+    t2.title AS title2,
+    levenshtein(t1.question, t2.title) AS distance
+FROM
+    bronze_questions t1
+CROSS JOIN
+    silver_proposals t2
+WHERE
+    levenshtein(t1.question, t2.title) < 5
+"""
+"# Proposal Similarity"
+proposal_similarity = con.sql(proposal_similarity_query).df()
+proposal_similarity
+
 for table in utils.all_tables():
     table_data = con.sql(f"SELECT * FROM {table}").df()
     "## " + table
