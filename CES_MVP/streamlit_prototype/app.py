b647a5e3ee2f6f17c9165168657b19c98e945d48
Roman Preuß <romanpre@gmail.com>
2024-06-13 19:26:26 +0200

    Restructure data loading page

--- CES_MVP/streamlit_prototype/app.py
+++ CES_MVP/streamlit_prototype/app.py
@@ -45,22 +45,6 @@ def manual_voting_xlsx_upload() -> bool:
 
 # Page content starts here
 
-"## Danger Zone"
-
-if st.button("Reset local database", type="primary"):
-    con.execute("USE demo;")
-    con.execute("DROP SCHEMA db CASCADE;")
-    con.execute("CREATE SCHEMA IF NOT EXISTS db;")
-    con.execute("USE db;")
-    """
-    Database successfully reset
-    """
-
-if not utils.mandatory_tables_loaded():
-    utils.hide_sidebar(True)
-else:
-    utils.hide_sidebar(False)
-
 """
 # Data Management
 Load the data into the local database and prepare it for analysis.
@@ -75,78 +59,87 @@ Make sure the excel file provides the following sheets (case-sensitive):
 
 voting_portal_loaded = manual_voting_xlsx_upload()
 
-if not voting_portal_loaded:
-    st.stop()
+if voting_portal_loaded:
+    "## Proposal Portal - Data (API)"
 
-"## Proposal Portal - Data (API)"
+    do_refetch_data = st.checkbox("Refetch from API", value=True)
+    fake_user_collection_ids = st.checkbox("Fake user collection ids", value=False)
 
-do_refetch_data = st.checkbox("Refetch from API", value=True)
-fake_user_collection_ids = st.checkbox("Fake user collection ids", value=False)
+    if st.button("Fetch from API"):
+        
+        progress_text = "Fetching general vorting portal data. Please wait."
+        progress_bar = st.progress(0, text=progress_text)
 
-if st.button("Fetch from API"):
-    
-    progress_text = "Fetching general vorting portal data. Please wait."
-    progress_bar = st.progress(0, text=progress_text)
+        deep_funding_api.load_rounds_and_pools_connection(refetch=do_refetch_data)
+        progress_bar.progress(0.5, text=progress_text)
 
-    deep_funding_api.load_rounds_and_pools_connection(refetch=do_refetch_data)
-    progress_bar.progress(0.5, text=progress_text)
+        deep_funding_api.load_pools(refetch=do_refetch_data)
+        progress_bar.progress(1.0, text=progress_text)
 
-    deep_funding_api.load_pools(refetch=do_refetch_data)
-    progress_bar.progress(1.0, text=progress_text)
+        
+        deep_funding_api.load_users(
+            refetch=do_refetch_data,
+            progress_updater=__progress_updater("Fetching users from voting portal. Please wait.")
+        )
 
-    
-    deep_funding_api.load_users(
-        refetch=do_refetch_data,
-        progress_updater=__progress_updater("Fetching users from voting portal. Please wait.")
-    )
+        deep_funding_api.load_comments(
+            refetch=do_refetch_data,
+            progress_updater=__progress_updater("Fetching comments from voting portal. Please wait.")
+        )
 
-    deep_funding_api.load_comments(
-        refetch=do_refetch_data,
-        progress_updater=__progress_updater("Fetching comments from voting portal. Please wait.")
-    )
+        deep_funding_api.load_proposals(
+            refetch=do_refetch_data,
+            progress_updater=__progress_updater("Fetching proposals from voting portal. Please wait.")
+        )
+        
+        deep_funding_api.load_milestones(
+            refetch=do_refetch_data,
+            progress_updater=__progress_updater("Fetching milestones from voting portal. Please wait.")
+        )
+        
+        deep_funding_api.load_reviews(
+            refetch=do_refetch_data,
+            progress_updater=__progress_updater("Fetching reviews from voting portal. Please wait.")
+        )
+        
+        deep_funding_api.load_comment_votes(
+            refetch=do_refetch_data,
+            progress_updater=__progress_updater("Fetching comment votes from voting portal. Please wait.")
+        )
 
-    deep_funding_api.load_proposals(
-        refetch=do_refetch_data,
-        progress_updater=__progress_updater("Fetching proposals from voting portal. Please wait.")
-    )
-    
-    deep_funding_api.load_milestones(
-        refetch=do_refetch_data,
-        progress_updater=__progress_updater("Fetching milestones from voting portal. Please wait.")
-    )
-    
-    deep_funding_api.load_reviews(
-        refetch=do_refetch_data,
-        progress_updater=__progress_updater("Fetching reviews from voting portal. Please wait.")
-    )
-    
-    deep_funding_api.load_comment_votes(
-        refetch=do_refetch_data,
-        progress_updater=__progress_updater("Fetching comment votes from voting portal. Please wait.")
-    )
-
-    if utils.table_exists("stg_vp_ratings"):
-        models.load(con, 'models/intermediate/int_proposal_mapping.sql')
-        models.load(con, 'models/intermediate/int_ratings.sql')
+        if utils.table_exists("stg_vp_ratings"):
+            models.load(con, 'models/intermediate/int_proposal_mapping.sql')
+            models.load(con, 'models/intermediate/int_ratings.sql')
 
 
-        models.load(con, 'models/marts/proposals.sql')
-        if fake_user_collection_ids:
-            models.load(con, 'models/marts/users_fake.sql', model_name='users')
-        else:
-            models.load(con, 'models/marts/users.sql')
-        models.load(con, 'models/marts/entropy.sql')
-        models.load(con, 'models/marts/vote_results.sql')
+            models.load(con, 'models/marts/proposals.sql')
+            if fake_user_collection_ids:
+                models.load(con, 'models/marts/users_fake.sql', model_name='users')
+            else:
+                models.load(con, 'models/marts/users.sql')
+            models.load(con, 'models/marts/entropy.sql')
+            models.load(con, 'models/marts/vote_results.sql')
 
-        # current voting algorithm
-        models.load(con, 'models/intermediate/int_user_contribution_engagement_score.sql')
-        models.load(con, 'models/intermediate/int_reputation_voting_weight.sql')
-        models.load(con, 'models/intermediate/int_agix_reward.sql')
+            # current voting algorithm
+            models.load(con, 'models/intermediate/int_user_contribution_engagement_score.sql')
+            models.load(con, 'models/intermediate/int_reputation_voting_weight.sql')
+            models.load(con, 'models/intermediate/int_agix_reward.sql')
 
-        models.load(con, 'models/marts/voting_weight.sql')
+            models.load(con, 'models/marts/voting_weight.sql')
 
+        """
+        Data successfully loaded
+        """
+
+"## Danger Zone"
+
+if st.button("Reset local database", type="primary"):
+    con.execute("USE demo;")
+    con.execute("DROP SCHEMA db CASCADE;")
+    con.execute("CREATE SCHEMA IF NOT EXISTS db;")
+    con.execute("USE db;")
     """
-    Data successfully loaded
+    Database successfully reset
     """
 
 if not utils.mandatory_tables_loaded():
