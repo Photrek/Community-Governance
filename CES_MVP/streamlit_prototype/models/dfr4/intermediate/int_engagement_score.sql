857b33dfd3930a142cba2e49542a6ed4c2589592
Roman Preuß <romanpre@gmail.com>
2024-06-16 19:10:03 +0200

    Add review count to reviews overall rating

--- CES_MVP/streamlit_prototype/models/dfr4/intermediate/int_engagement_score.sql
+++ CES_MVP/streamlit_prototype/models/dfr4/intermediate/int_engagement_score.sql
@@ -5,15 +5,19 @@ select
     c.comment_count,
     cv.upvote_count,
     cv.downvote_count,
-    
+
     -- add count of reviews
     count(r.reviewer_id) as review_count,
 
+    -- add average overall_rating score
+    coalesce(avg(r.overall_rating), 0) as average_review_overall_rating,
+    
     -- TODO: add more signals as defined in https://docs.google.com/document/d/1Kz7YPG03eaqYJlRbg0FnQlExxM5nb-57KM1sE53WfbM
     (c.comment_count * 3.0) 
     + (cv.upvote_count * 2.0) 
     + (cv.downvote_count * -3.0)
     + (review_count * 2)
+    + (average_review_overall_rating * 2)
         as engagement_score
 from
     int_comment_votes as cv
